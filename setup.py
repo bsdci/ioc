734d26bd1ec09241cb3214de7bd475f06f0e47cc
Stefan Grönke <stefan@gronke.net>
2019-08-08 06:49:47 +0000

    bump version to v0.8.2

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ if sys.version_info < (3, 6):
 setup(
     name='ioc_cli',
     license='BSD',
-    version='0.8.1',
+    version='0.8.2',
     description='A Python library to manage jails with iocage',
     keywords='FreeBSD jail iocage ioc',
     author='ioc Contributors',
